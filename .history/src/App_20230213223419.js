076f55ae751f1e29a537cad379bfd1586df79a6f
maiduchuy321 <maiduchuy321@gmail.com>
2023-02-13 22:41:52 +0700

    add header

--- .history/src/App_20230213223419.js
+++ .history/src/App_20230213223419.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
+import Header from "../components/Header/Header";
 const App = () => {
   return (
     <div className="app-container">
